f0b48f4f7cc1ed4700646792b673cebf3b3a1f86
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-06-17 00:11:55 +0600

    add some style

--- src/pages/Classes/Classes/Classes.jsx
+++ src/pages/Classes/Classes/Classes.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import useTitle from "../../../Hook/UseTitle";
 
 const Classes = () => {
-  useTitle("All Class")
+  useTitle("All Class");
   const [classes, setClasses] = useState([]);
   const [currentRole, setCurrentRole] = useState("");
   const { user } = useContext(AuthContext);
@@ -100,16 +100,20 @@ const Classes = () => {
           {classes.map((popular, i) => (
             <div
               key={i}
-              className={`card card-compact md:w-96 mx-4 bg-base-100 shadow-xl ${
-                popular?.availableSeats === 0 && "bg-red-100"
+              className={`card-compact rounded-lg md:w-96 mx-4 bg-base-100 shadow-xl  ${
+                popular?.availableSeats === 0 && "bg-red-300 text-black"
               }`}
             >
               <figure>
-                <img src={popular?.imgURL} alt="Shoes" />
+                <img
+                  className="w-full h-64 shadow-lg shadow-[#fff]"
+                  src={popular?.imgURL}
+                  alt="Shoes"
+                />
               </figure>
               <div className="card-body relative">
                 {popular?.availableSeats === 0 && (
-                  <p className="bg-[#1dcdbc] absolute -top-8 py-2 px-3 font-semibold text-gray-900 shadow-lg">
+                  <p className="bg-[#1dcdbc] absolute -top-8 py-2 px-3 font-semibold text-gray-900 shadow-lg uppercase">
                     {" "}
                     Full fill up Seats
                   </p>
